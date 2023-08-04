56387617d5eec6b8667fb4583132cb72fd31cceb
Endre Solem <esolem@gmail.com>
2023-08-04 22:13:12 +0200

    solved port type problem

--- helpers/db.ts
+++ helpers/db.ts
@@ -4,9 +4,10 @@ import mysql from "serverless-mysql";
 const db = mysql({
   config: {
     host: process.env.MYSQL_HOST,
-    database: process.env.MYSQL_DATABASE,
+    port: parseInt(process.env.MYSQL_PORT),
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
   },
 });
 
