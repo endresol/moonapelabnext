ec4d1cbafccd6118a89fd5ef76ac0284e0f52346
Endre Solem <esolem@gmail.com>
2023-08-04 21:49:22 +0200

    adding a lot mostly because of prisma not working

--- pages/api/ape_nft/[address].ts
+++ pages/api/ape_nft/[address].ts
@@ -1,32 +1,30 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("getting data:", req.query.address);
-  
+
   const prisma = new PrismaClient();
   if (req.method === "GET") {
     const address = req.query.address;
-    
+
     const nfts = await prisma.nfts_apenft.findMany({
       select: {
         nft_id: true,
       },
       where: {
-        owner : address as string,
-        }
-      }
-    );
-    
-    const nftIds = nfts.map(nft => nft.nft_id);
+        owner: address as string,
+      },
+    });
+
+    const nftIds = nfts.map((nft) => nft.nft_id);
     res.status(200).json({ nft_ids: nftIds });
-    
   }
 }
 
 // async function main(address) {
 //   // ... you will write your Prisma Client queries here
-//   const allUsers = await 
+//   const allUsers = await
 //   console.log(allUsers)
 // }
 
@@ -39,6 +37,5 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 //     await prisma.$disconnect()
 //     process.exit(1)
 //   })
-
 
 export default handler;
--- pages/api/ape_nft/index.ts
+++ pages/api/ape_nft/index.ts
@@ -1,27 +1,37 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
 
-function handler(req, res) { 
+import excuteQuery from "../../../helpers/db";
+
+async function handler(req, res) {
+  try {
+    console.log("req nom", req.body);
+    const result = await excuteQuery({
+      query: "SELECT * FROM nfts_apenft",
+      values: [],
+    });
+    console.log("ttt", result);
+  } catch (error) {
+    console.log(error);
+  }
+
   res.status(200).json({ message: "this is it" });
-
 }
-
 
 async function main() {
-  // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.nfts_apenft.findMany();
-  console.log(allUsers)
+  // // ... you will write your Prisma Client queries here
+  // const allUsers = await prisma.nfts_apenft.findMany();
+  // console.log(allUsers);
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
-
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
 
 export default handler;
