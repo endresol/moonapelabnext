0b80b1110acf229e18d2aee0db60cc66673568e0
Endre Solem <esolem@gmail.com>
2023-12-26 10:56:05 +0100

    added walletguard logo and link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,69 +3,91 @@ import Image from "next/image";
 import Link from "next/link";
 import { Web3Button } from "../components";
 
-
 export function NavBar() {
   return (
-     <nav className="flex-no-wrap relative flex w-full items-center justify-between py-4 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start"
-  data-te-navbar-ref>
-  <div className="flex w-full flex-wrap items-center justify-between px-6">
-    <button
-      className="block border-0 bg-transparent py-2 px-2.5 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
-      type="button"
-      data-te-collapse-init
-      data-te-target="#navbarSupportedContent1"
-      aria-controls="navbarSupportedContent1"
-      aria-expanded="false"
-      aria-label="Toggle navigation">
-      <span className="[&>svg]:w-7">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="h-7 w-7">
-          <path
-            fillRule="evenodd"
-            d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-            clipRule="evenodd" />
-        </svg>
-      </span>
-    </button>
-    <div
-      className="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
-      id="navbarSupportedContent1"
-      data-te-collapse-item>
-      <a
-        className="mt-2 mr-2 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mt-0"
-        href="#">
-        <Image
-          src="/MAL_LOGO.svg"
-          height= "45"
-          width="45"
-          alt=""
-          loading="lazy" />
-      </a>
-      {/* <!-- Left links --> */}
-      <ul
-        className="list-style-none mr-auto flex flex-col pl-0 lg:flex-row"
-        data-te-navbar-nav-ref>
-        <li className="lg:pr-2" data-te-nav-item-ref>
-          <Link
-            className="text-neutral-200 hover:text-neutral-400 focus:text-neutral-400 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400"
-            href="https://moonapelab.io"
-            data-te-nav-link-ref
-            >WEBSITE</Link>
-        </li>
-      </ul>
-      {/* <!-- Left links --> */}
-    </div>
-    {/* <!-- Collapsible wrapper --> */}
+    <nav
+      className='flex-no-wrap relative flex w-full items-center justify-between py-4 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start'
+      data-te-navbar-ref
+    >
+      <div className='flex w-full flex-wrap items-center justify-between px-6'>
+        <button
+          className='block border-0 bg-transparent px-2.5 py-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden'
+          type='button'
+          data-te-collapse-init
+          data-te-target='#navbarSupportedContent1'
+          aria-controls='navbarSupportedContent1'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='[&>svg]:w-7'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              fill='currentColor'
+              className='h-7 w-7'
+            >
+              <path
+                fillRule='evenodd'
+                d='M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z'
+                clipRule='evenodd'
+              />
+            </svg>
+          </span>
+        </button>
+        <div
+          className='!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto'
+          id='navbarSupportedContent1'
+          data-te-collapse-item
+        >
+          <a
+            className='mr-2 mt-2 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mt-0'
+            href='#'
+          >
+            <Image
+              src='/MAL_LOGO.svg'
+              height='45'
+              width='45'
+              alt=''
+              loading='lazy'
+            />
+          </a>
+          {/* <!-- Left links --> */}
+          <ul
+            className='list-style-none mr-auto flex flex-col pl-0 lg:flex-row'
+            data-te-navbar-nav-ref
+          >
+            <li className='lg:pr-2' data-te-nav-item-ref>
+              <Link
+                className='text-neutral-200 hover:text-neutral-400 focus:text-neutral-400 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400'
+                href='https://moonapelab.io'
+                data-te-nav-link-ref
+              >
+                WEBSITE
+              </Link>
+            </li>
+          </ul>
+          {/* <!-- Left links --> */}
+        </div>
+        {/* <!-- Collapsible wrapper --> */}
 
-    {/* <!-- Right elements --> */}
-    <div className="relative flex items-center">
-      <Web3Button />
-    </div>
-    {/* <!-- Right elements --> */}
-  </div>
-</nav>
-  )
+        {/* <!-- Right elements --> */}
+        <div className='relative flex items-center'>
+          <a
+            href='https://www.walletguard.app/'
+            target='_blank'
+            className='mr-3'
+          >
+            <Image
+              src='/wallet-guard-logo.png'
+              height='35'
+              width='125'
+              alt='WalletGuard Partner'
+            />
+          </a>
+          <Web3Button />
+        </div>
+        {/* <!-- Right elements --> */}
+      </div>
+    </nav>
+  );
 }
