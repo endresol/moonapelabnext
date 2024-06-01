7623fcef259db4e99aa5e9ac609d70d80eaf4280
Endre Solem <esolem@gmail.com>
2024-06-01 14:28:20 +0200

    removing comment with old dbname in

--- pages/api/ape_nft/[address].ts
+++ pages/api/ape_nft/[address].ts
@@ -1,22 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-// import { PrismaClient } from "@prisma/client";
 import executeQuery from "../../../helpers/db";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("getting data:", req.query.address);
 
-  // const prisma = new PrismaClient();
   if (req.method === "GET") {
     const address = req.query.address;
-
-    // const nfts = await prisma.nfts_apenft.findMany({
-    //   select: {
-    //     nft_id: true,
-    //   },
-    //   where: {
-    //     owner: address as string,
-    //   },
-    // });
 
     const result = await executeQuery({
       query: {
@@ -26,25 +15,8 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       values: [req.query.address],
     });
 
-    // const nftIds = result.map((nft) => nft.nft_id);
     res.status(200).json({ nft_ids: result });
   }
 }
-
-// async function main(address) {
-//   // ... you will write your Prisma Client queries here
-//   const allUsers = await
-//   console.log(allUsers)
-// }
-
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
 
 export default handler;
--- pages/api/get_ticket/[address].ts
+++ pages/api/get_ticket/[address].ts
@@ -1,22 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
-// import { PrismaClient } from "@prisma/client";
+
 import executeQuery from "../../../helpers/db";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("getting data:", req.query.address);
 
-  // const prisma = new PrismaClient();
   if (req.method === "GET") {
     const address = req.query.address;
-
-    // const tickets = await prisma.mal_raffle_purchase.aggregate({
-    //   _sum: {
-    //     quantity: true,
-    //   },
-    //   where: {
-    //     address: address as string,
-    //   },
-    // });
 
     const result = await executeQuery({
       query:
@@ -31,21 +21,5 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       .json({ mytickets: result[0].mytickets ? result[0].mytickets : 0 });
   }
 }
-
-// async function main(address) {
-//   // ... you will write your Prisma Client queries here
-//   const allUsers = await
-//   console.log(allUsers)
-// }
-
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
 
 export default handler;
